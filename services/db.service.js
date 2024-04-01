b0d2067cd0d5fd4c7a3daa79055d7cbfbd8807ef
EdenAlmighty <edengilady1@gmail.com>
2024-04-01 23:59:57 +0300

    Build v1.1.1 Update mongodb

--- services/db.service.js
+++ services/db.service.js
@@ -1,4 +1,40 @@
-import MongoClient from 'mongodb'
+// import MongoClient from 'mongodb'
+
+// import { config } from '../config/index.js'
+// import { logger } from './logger.service.js'
+
+// export const dbService = {
+//     getCollection
+// }
+
+// var dbConn = null
+
+// async function getCollection(collectionName) {
+//     try {
+//         const db = await _connect()
+//         const collection = await db.collection(collectionName)
+//         return collection
+//     } catch (err) {
+//         logger.error('Failed to get Mongo collection', err)
+//         throw err
+//     }
+// }
+
+// async function _connect() {
+//     if (dbConn) return dbConn
+//     try {
+//         const client = await MongoClient.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+//         const db = client.db(config.dbName)
+//         dbConn = db
+//         return db
+//     } catch (err) {
+//         logger.error('Cannot Connect to DB', err)
+//         throw err
+//     }
+// }
+
+import mongoDB from 'mongodb'
+const { MongoClient } = mongoDB
 
 import { config } from '../config/index.js'
 import { logger } from './logger.service.js'
@@ -11,7 +47,7 @@ var dbConn = null
 
 async function getCollection(collectionName) {
     try {
-        const db = await _connect()
+        const db = await connect()
         const collection = await db.collection(collectionName)
         return collection
     } catch (err) {
@@ -20,10 +56,12 @@ async function getCollection(collectionName) {
     }
 }
 
-async function _connect() {
+async function connect() {
     if (dbConn) return dbConn
     try {
-        const client = await MongoClient.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+        // (node:9304) [MONGODB DRIVER] Warning: useNewUrlParser is a deprecated option: useNewUrlParser, useUnifiedTopology has no effect since Node.js Driver version 4.0.0 and will be removed in the next major version
+        // const client = await MongoClient.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+        const client = await MongoClient.connect(config.dbURL)
         const db = client.db(config.dbName)
         dbConn = db
         return db
@@ -32,3 +70,7 @@ async function _connect() {
         throw err
     }
 }
+
+
+
+
